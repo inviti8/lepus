a128a16fdfbd24030efc40807dcfd82a87d19f0e
Chukwuka Rosemary Uche <chukwukarosemary2020@gmail.com>
2026-03-24 19:40:04 +0000

    Bug 1846829 - Remove the max-history-rows pref used in Fx View r=kcochrane,fxview-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D289063

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3422,8 +3422,9 @@ pref("browser.places.interactions.enabled", true);
 pref("browser.firefox-view.feature-tour", "{\"screen\":\"FIREFOX_VIEW_SPOTLIGHT\",\"complete\":false}");
 // Number of times the user visited about:firefoxview via the button/hidden tab in the past 30 days
 pref("browser.firefox-view.button-clicks", "{\"count\":0,\"lastCountTime\":\"\"}");
-// Maximum number of rows to show on the "History" page (0 = unlimited).
-pref("browser.firefox-view.max-history-rows", 0);
+
+// Enables virtual list functionality in Firefox View.
+pref("browser.firefox-view.virtual-list.enabled", true);
 
 // If the user has seen the pdf.js feature tour this value reflects the tour
 // message id, the id of the last screen they saw, and whether they completed the tour
